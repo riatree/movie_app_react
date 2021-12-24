8b418d1112345b83fb4290e2c131ee93a6c7b44d
scriptwhale <scriptwhale@gmail.com>
2021-12-24 14:23:15 +0900

    Modified for React Router v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import Home from './routers/Home';
 import './App.css';
+import {HashRouter ,Routes ,Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-  return <Home />
+  return (
+    <HashRouter >
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </HashRouter >
+  )
 }
+
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className='about__container'>
+            <sapn>
+                "Freedom is the freedom to say that two plus two make four. If that is grante, all else follows."
+            </sapn>
+            <span>-George Orwell, 1984</span>
+        </div>
+    );
+}
+
+export default About;
